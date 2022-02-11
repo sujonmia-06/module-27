8a4c2714cb494d2fa62002b30b4d18024bde4b04
sujonmia <sujonmia.0612@gmail.com>
2022-02-11 18:23:48 +0600

    function added for total field amount for both deposit and withdraw

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,5 @@
-// deposit input field function
-function getInputValue (inputID){
+// input field function
+function getInputValue(inputID) {
     // input amount in input field and make it float value
     const input = document.getElementById(inputID);
     const newAmount = parseFloat(input.value);
@@ -8,16 +8,21 @@ function getInputValue (inputID){
 
     return newAmount;
 }
+// update total field amount function
+function updateTotalField(totalFieldID, newAmount) {
+    // amount and make it float value and update total amount
+    const total = document.getElementById(totalFieldID);
+    const previousTotal = parseFloat(total.innerText);
+    total.innerText = previousTotal + newAmount;
+}
 
 // deposit amount and update total balance amount
-document.getElementById('deposit-btn').addEventListener('click', function() {
-    // function calling
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    // function calling: input field function
     const newDepositAmount = getInputValue('deposit-input');
-    // deposit amount and make it float value and update total deposit amount
-    const depositTotal = document.getElementById('deposit-total');
-    const previousDepositTotal = parseFloat(depositTotal.innerText);
-    depositTotal.innerText =  previousDepositTotal + newDepositAmount;
-    
+    // function calling: update total field amount function
+    updateTotalField('deposit-total', newDepositAmount);
+
     // previous balance amount and new deposit amount adding
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceTotal = parseFloat(balanceTotal.innerText);
@@ -25,14 +30,12 @@ document.getElementById('deposit-btn').addEventListener('click', function() {
 });
 //////////////////////////////////////////////////////////////////////////////
 // withdraw amount and update total balance amount
-document.getElementById('withdraw-btn').addEventListener('click', function() {
-    // function calling
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    // function calling: input field function
     const newWithdrawAmount = getInputValue('withdraw-input');
-    // withdraw amount and make it float value and update total withdraw amount
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const previousWithdrawTotal = parseFloat(withdrawTotal.innerText);
-    withdrawTotal.innerText = previousWithdrawTotal + newWithdrawAmount;
-    
+    // function calling: update total field amount function
+    updateTotalField('withdraw-total', newWithdrawAmount);
+
     // previous balance amount and new withdraw amount deducting
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceTotal = parseFloat(balanceTotal.innerText);
